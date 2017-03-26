d68a6fdd12245b0b1dfb5bfea53b2e619d038d8f
Valdiney França <valdiney.2@hotmail.com>
2017-03-25 22:44:11 -0300

    Update acenderEapagarLeds.ino

--- acenderEapagarLeds.ino
+++ acenderEapagarLeds.ino
@@ -13,45 +13,52 @@
 int leds[4] = {8,9,10,11};
 
 /**
+ * int listLength: Tamanho da lista [leds]
  * int sleep: Temporizador, ou seja, delay
+ * 
  * Essa função é utilizada para ligar todos os leds 
  * cujas posições estão alocadas na lista [leds]
  */
-void highLeds(int sleep) {
-  for (int i = 0; i <= 40; i++) {
+void highLeds(int listLength, int sleep) {
+  for (int i = 0; i <= listLength; i++) {
    digitalWrite(leds[i], HIGH);
    delay(sleep);
   }
 }
 
 /**
+ * int listLength: Tamanho da lista [leds]
  * int sleep: Temporizador, ou seja, delay
+ * 
  * Essa função é utilizada para desligar todos os leds 
  * cujas posições estão alocadas na lista [leds]
  */
-void lowLeds(int sleep) {
-  for (int i = 0; i <= 40; i++) {
+void lowLeds(int listLength, int sleep) {
+  for (int i = 0; i <= listLength; i++) {
     digitalWrite(leds[i], LOW);
     delay(sleep);
   }
 }
 
 /**
+ * int listLength: Tamanho da lista [leds]
+ * int listLength: Tamanho da lista [leds]
+ * 
  * Essa função é usada para setar a Constant OUTPUT 
  * para todas as numerações dos leds na lista [leds]
  */
-void startLeds() {
-  for (int i = 0; i <= 40; i++) {
+void startLeds(int listLength) {
+  for (int i = 0; i <= listLength; i++) {
     digitalWrite(leds[i], OUTPUT);
   }
 }
 
 void setup() {
-  startLeds();
+  startLeds(4);
 }
 
 void loop() {
-  highLeds(100);
+  highLeds(4, 100);
   delay(100);
-  lowLeds(100);
+  lowLeds(4, 100);
 }
